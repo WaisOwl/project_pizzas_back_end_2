015a3e4620be544c31910a841a37e46adc679efb
Thomas Oviedo <thomijuan@gmail.com>
2025-09-03 21:17:17 -0300

    implementacion de la interfaz del panel administrador

--- login.php
+++ login.php
@@ -26,8 +26,6 @@
   // uso de POO- Programacion orientada a objetos
   $stmt = $link->run($sql, [$usr, $usr, $hashed_pass]);
   $row=$stmt->fetch(PDO::FETCH_ASSOC);
-
-  if (!$row) {
 if (!$row) {
     ?>
         <div class="alert alert-danger">
@@ -48,9 +46,7 @@ if (!$row) {
         header("location:welcome.php");
       }
     }
-}
-?>
-
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js" integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q" crossorigin="anonymous"></script>
+    ?>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js" integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q" crossorigin="anonymous"></script>
 </body>
 </html>
